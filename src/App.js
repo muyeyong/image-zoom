2626ab11c5a68b3ca449355028c48f66bb4ca30c
muyeyong <xuyongshuaige@gmail.com>
2022-05-24 10:53:58 +0800

    更新redame

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ImageZoom from './component/ImageZoom.tsx';
 function App() {
   return (
     <div className="App" style={{ border: '1px solid red'}}>
-     <ImageZoom src={require('./assets/DJI_0926.jpeg')} />
+     <ImageZoom src={require('./assets/DJI_0926.jpeg')}></ImageZoom>
     </div>
   );
 }
